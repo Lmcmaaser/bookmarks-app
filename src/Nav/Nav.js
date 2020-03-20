4b414fe5f6630b2306aa563e587d5637f2c59d9d
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2020-03-19 23:47:47 -0700

    added propTypes and context to rating components

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function Nav(props) {
   return (
     <nav className='Nav'>
-      <button onClick={() => props.clickPage('list')}>
+      <Link to={'/'}>
         Bookmark List
-      </button>
+      </Link>
       {' '}
-      <button onClick={() => props.clickPage('add')}>
+      <Link to={'/add-bookmark'}>
         Add Bookmark
-      </button>
+      </Link>
     </nav>
   );
 }
